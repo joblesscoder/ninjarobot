7363e522ebfb2d66d8bbb8e38273b92bdc3df8bc
joblesscoder <axnikonov@gmail.com>
2020-12-29 21:45:36 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -10,23 +10,23 @@ import Persik from './panels/Persik';
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	const [popout, setPopout] = useState(null);
 
-	// useEffect(() => {
-	// 	bridge.subscribe(({ detail: { type, data }}) => {
-	// 		if (type === 'VKWebAppUpdateConfig') {
-	// 			const schemeAttribute = document.createAttribute('scheme');
-	// 			schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-	// 			document.body.attributes.setNamedItem(schemeAttribute);
-	// 		}
-	// 	});
-	// 	async function fetchData() {
-	// 		const user = await bridge.send('VKWebAppGetUserInfo');
-	// 		setUser(user);
-	// 		setPopout(null);
-	// 	}
-	// 	fetchData();
-	// }, []);
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
 
 	const go = e => {
 		setActivePanel(e.currentTarget.dataset.to);
